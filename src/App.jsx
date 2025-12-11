fd7e73c7c446963cd3503bbd0557a72d330e2be0
Bashar <basharabulaziz25@gmail.com>
2025-12-11 22:17:44 +0300

    add vercel file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import {CitiesProvider} from "./contexts/CitiesContext";
+import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
-
 
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import SpinnerFullPage from "./components/SpinnerFullPage";
-
 
 // import Homepage from "./pages/Homepage";
 // import Pricing from "./pages/Pricing";
@@ -20,13 +18,12 @@ import SpinnerFullPage from "./components/SpinnerFullPage";
 // import PageNotFound from "./pages/PageNotFound";
 // import Login from "./pages/Login";
 
-const Homepage = lazy(() => import('./pages/Homepage'));
-const Pricing = lazy(() => import('./pages/Pricing'));
-const Product = lazy(() => import('./pages/Product'));
-const AppLayout = lazy(() => import('./pages/AppLayout'));
-const PageNotFound = lazy(() => import('./pages/PageNotFound'));
-const Login = lazy(() => import('./pages/Login'));
-
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Login = lazy(() => import("./pages/Login"));
 
 // dist/index.html                   0.49 kB │ gzip:   0.31 kB
 // dist/assets/index-C0MHSRRJ.css   30.23 kB │ gzip:   5.03 kB
@@ -49,49 +46,40 @@ const Login = lazy(() => import('./pages/Login'));
 // dist/assets/Product-BWrmKvkK.js           0.86 kB │ gzip:   0.49 kB
 // dist/assets/Login-C7N0wCzn.js                    1.02 kB │ gzip:   0.54 kB
 // dist/assets/AppLayout-DHtivrKL.js157.14 kB │ gzip:  46.36 kB
-// dist/assets/index-CwNRPrqo.js           348.26 kB │ gzip: 102.75 
+// dist/assets/index-CwNRPrqo.js           348.26 kB │ gzip: 102.75
 
 function App() {
-  
-  return(
+  return (
     <AuthProvider>
       <CitiesProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/app">
           <Suspense fallback={<SpinnerFullPage />}>
             <Routes>
-              <Route index element={<Homepage />}/>
-              <Route path="pricing" element={<Pricing />}/>
-              <Route path="product" element={<Product />}/>
-              <Route path="login" element={<Login />}/>
-              <Route path="app" element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
+              <Route index element={<Homepage />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="product" element={<Product />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
                 }
-              > 
-                <Route 
-                  index
-                  element={<Navigate replace to="cities" />}
-                />
-                <Route 
-                  path="cities" 
-                  element={<CityList
-                  />}
-                />
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
                 <Route path="cities/:id" element={<City />} />
-                <Route 
-                  path="countries" 
-                  element={<CountryList/>}
-                />
-                <Route path="form" element={<Form />}/>
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
               </Route>
-              <Route path="*" element={<PageNotFound />}/>
+              <Route path="*" element={<PageNotFound />} />
             </Routes>
           </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
